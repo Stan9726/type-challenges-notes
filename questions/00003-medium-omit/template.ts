8df907ad4c9f644cdebd7d0930637719e423ef84
Stan <Stan9726@users.noreply.github.com>
2022-05-12 09:46:44 +0800

    improve: 00003 Omit

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,3 +1,4 @@
-type MyOmit<T extends Record<string, any>, K extends keyof T> = {
-  [P in keyof T as P extends K ? never : P]: T[P]
-}
+// type MyOmit<T, K extends keyof T> = {
+//   [P in keyof T as P extends K ? never : P]: T[P]
+// }
+type MyOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
