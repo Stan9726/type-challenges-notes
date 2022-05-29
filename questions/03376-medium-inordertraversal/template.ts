a405faa8d48dcdfd3c1ccc98edc079b2fc4cc3a8
Stan <Stan9726@users.noreply.github.com>
2022-05-29 16:43:08 +0800

    solve: 03376 InorderTraversal

---
+++ questions/03376-medium-inordertraversal/template.ts
@@ -0,0 +1,8 @@
+interface TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+}
+type InorderTraversal<T extends TreeNode | null> = [T] extends [TreeNode]
+  ? [...InorderTraversal<T['left']>, T['val'], ...InorderTraversal<T['right']>]
+  : []
